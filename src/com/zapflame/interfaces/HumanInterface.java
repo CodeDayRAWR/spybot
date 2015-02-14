9d3178b5ab8dcd8ae30d273037439b4b1eb95985
Mitch <mitch.cro@gmail.com>
2015-02-14 13:58:53 -0600

    Minor changes to interfaces

--- src/com/zapflame/interfaces/HumanInterface.java
+++ src/com/zapflame/interfaces/HumanInterface.java
@@ -1,7 +1,24 @@
 package com.zapflame.interfaces;
 
-public class HumanInterface extends PlayerInterface {
+import com.zapflame.core.GameStateMessage;
+import com.zapflame.core.Message;
+import com.zapflame.core.Move;
+
+public class HumanInterface implements PlayerInterface {
+
+  public Move getMove() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void update(Message m) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void update(GameStateMessage gsm) {
+    // TODO Auto-generated method stub
+
+  }
 
 }
-
-
