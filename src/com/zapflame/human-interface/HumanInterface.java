117c2ba20680c6e62ca6a25d73a1d18b134820ac
rschaosid <rschaosid@gmail.com>
2015-02-14 21:42:36 -0600

    add HumanInterface

--- src/com/zapflame/human-interface/HumanInterface.java
+++ src/com/zapflame/human-interface/HumanInterface.java
@@ -1,7 +1,13 @@
-package com.zapflame.human-interface
+package com.zapflame.human-interface;
 
-import com.zapflame.core.Interface
+import com.zapflame.core.Interface;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class HumanInterface {
-  
+public class HumanInterface extends Application {
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("Spybot");
+    primaryStage.show();
+  }
 }
